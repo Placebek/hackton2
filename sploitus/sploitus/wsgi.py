75b2601a5e86a3422ff07e521fe05aa066a2c592
sakenzi <ermekulysaken8@gmail.com>
2024-12-06 15:16:15 +0600

    Пустой шаблон

---
+++ sploitus/sploitus/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sploitus project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sploitus.settings')
+
+application = get_wsgi_application()
